c66b6eec7ee8ef0be385c41757f293d7263ff5d8
HaiDang <19521319@gm.uit.edu.vn>
2021-12-19 20:47:51 +0700

    add view contact

---
+++ Courses_MVC/Courses_MVC/Areas/Admin/Pages/User/DeleteUser.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Courses_MVC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Courses_MVC.Areas.Admin.Pages.User
+{
+    public class DeleteUserModel : PageModel
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public DeleteUserModel(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public AppUser user { get; set; }
+        public async Task<IActionResult> OnGet(string userId)
+        {
+            if (userId == null)
+                return NotFound("Không tìm thấy role");
+            user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound("Không tìm thấy role");
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(string userId)
+        {
+            if (userId == null)
+                return NotFound("Không tìm thấy role");
+            user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                return NotFound("Không tìm thấy role");
+
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                StatusMessage = $"Bạn vừa xóa user có UserName: {user.UserName}";
+                return RedirectToPage("./Index");
+            }
+            else
+            {
+                result.Errors.ToList().ForEach(error => {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                });
+            }
+            return Page();
+        }
+    }
+}
